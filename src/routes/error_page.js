577ab605356d8829c7d8e46cc35b844250499888
Sophie <sophie.garreta@gmail.com>
2024-01-09 10:53:46 +0100

    error page style and redirecty

--- src/routes/error_page.js
+++ src/routes/error_page.js
@@ -1,16 +1,18 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, Link } from "react-router-dom";
+import '../style/error.css';
 
 const ErrorPage = () => {
   const error = useRouteError();
   console.error(error);
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
+    <div className="error">
+      <h1 className="error__title">Oops!</h1>
+      <p className="error__txt">Sorry, an unexpected error has occurred.</p>
+      <p className="error__spec">
         <i>{error.statusText || error.message}</i>
       </p>
+      <Link className="error__link link" to="/" >Go back to login page</Link>
     </div>
   );
 }
