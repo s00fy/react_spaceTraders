ba46d14c463f4a2cfae5e4d852ec51ad77912db1
Sophie <sophie.garreta@gmail.com>
2024-01-09 09:30:15 +0100

    devdep babel

--- src/components/ships/Fuel.js
+++ src/components/ships/Fuel.js
@@ -1,3 +1,5 @@
+import '../../style/fuel.css';
+
 const Fuel = (props)=>{
 
     const Fuel = props.fuel;
@@ -5,16 +7,13 @@ const Fuel = (props)=>{
     return(
         <>
         { Fuel ? (
-        <td className='fleetTable__column'>
-        <div className='fleetTable__fuel'>
-          <p className='fleetTable__fuel__title'>Fuel</p>
-          <div className='fleetTable__fuel__progress'>
+        <td className='fuel'>
+        <div className='fuel__card'>
+          <p className='fuel__title'>Fuel</p>
+          <div className='fuel__progress'>
             <div className="semi-donut margin" /* style="--percentage :20; --fill: #FF3D00 ;" */>
             {Fuel.current}
             </div>
-          </div>
-          <div className='fleetTable__fuel__list'>
-              
           </div>
         </div>
       </td>
